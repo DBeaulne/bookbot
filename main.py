9f5a8e9681cef2a1fa7e2038b1dbbaace497bb36
DBeaulne <daniel.beaulne.dev@gmail.com>
2025-10-26 16:07:58 -0400

    create sorted list of dictionaries from unsorted list

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count, count_each_character
+from stats import get_word_count, count_each_character, character_sort
 
 """ 
 ## This was my solution for Bookbot, Chapter 2, Lesson 3
@@ -17,16 +17,23 @@ def main():
 def get_book_text(path):
   with open(path) as f:
     return f.read()
-  
+   
 def main():
   filepath = "books/frankenstein.txt"
   text = get_book_text(filepath)
-  # print(text)
+  print("============= BOOKBOT =============")
+  print(f"Analyzing book fount at", filepath,"...")
+
   word_count = get_word_count(text)
+  print("------------ Word Count -----------")
   print(f"Found {word_count} total words")
 
   char_count = count_each_character(text.lower())
-  print(char_count)
-  
+
+  sorted_list = character_sort(char_count)
+  print("--------- Character Count ---------")
+  for char in sorted_list:
+    if char['char'].isalpha() == True:
+      print(f"{char['char']}: {char['num']}")
 
 main()
--- stats.py
+++ stats.py
@@ -10,5 +10,24 @@ def count_each_character(text):
       char_dict[char] += 1
     else:
       char_dict[char] = 1
-  
   return char_dict
+
+
+
+def character_sort(character_dict):
+  def sort_on(items):
+    return items["num"]
+  
+  list_of_characters = []
+
+  for key, value in character_dict.items():
+    new_dict = {'char': key, 'num': value}
+    list_of_characters.append(new_dict)
+
+  # print(f"Unsorted List: ", list_of_characters)
+  list_of_characters.sort(reverse=True, key=sort_on)
+
+  return list_of_characters
+
+
+
