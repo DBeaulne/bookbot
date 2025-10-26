c1fc4b618e2af3b0362bf1d48b51cc29e3eefbbb
DBeaulne <daniel.beaulne.dev@gmail.com>
2025-10-26 17:51:49 -0400

    declared variable to use dynamic path

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
 from stats import (get_word_count, count_each_character, character_sort)
 import sys
 
-""" 
-## This was my solution for Bookbot, Chapter 2, Lesson 3
-def get_book_text(filepath):
-  contents = filepath.read()
-  return contents
-
-def main():
-  filepath = "books/frankenstein.txt"
-  with open(filepath) as f:
-    print(get_book_text(f))
-"""
-
-# Boot.dev solution
-
 def get_book_text(path):
   with open(path) as f:
     return f.read()
    
 def main():
-  if len(sys.argv) != 2:
+  if len(sys.argv) < 2:
     print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
+  book_path = sys.argv[1]
 
-  text = get_book_text(sys.argv[1])
+  text = get_book_text(book_path)
   print("============= BOOKBOT =============")
-  print(f"Analyzing book fount at {sys.argv[1]}...")
+  print(f"Analyzing book fount at {book_path}...")
 
   word_count = get_word_count(text)
   print("------------ Word Count -----------")
